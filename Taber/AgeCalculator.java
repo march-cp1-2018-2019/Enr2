fff79c672ca7b8b008ba12e84f7502facd2f5a1b
nortoha <39999692+nortoha@users.noreply.github.com>
2019-02-03 15:26:19 -0600

    Notes from the teacher

--- Taber/AgeCalculator.java
+++ Taber/AgeCalculator.java
@@ -1,12 +1,13 @@
-package dates;
-
 import java.util.Scanner;
+// TEACHER NOTES: you are missing some imports here from the original file.  
+// Don't delete these, you need them.
 
 public class AgeCalculator {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Age Calculator\n");
         
+        // TEACHER NOTES: this isn't compiling because the import statement is missing.
         LocalDateTime now = LocalDateTime.now();
         System.out.println(now);
         
@@ -19,10 +20,15 @@ public class AgeCalculator {
         
         
         @SuppressWarnings("resource")
+        // TEACHER NOTES: don't declare this again...its already on line 16
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your date of birth (YYYY-MM-DD): ");
         String dateOfBirthString = sc.nextLine();
         System.out.println();
+        
+        // TEACHER NOTES: It doesn't look like you finished this one. 
+        // I hope that going over this class in class helped you out.
+        // You can definitely turn this in again for full credit.
         
     }
     
